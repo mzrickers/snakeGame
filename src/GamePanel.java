df8dff19109d4e31b60d165134f3f08cee3b2956
Zach Rickers <mzrickers@gmail.com>
2022-04-16 08:12:52 +0200

    gamePanel setup

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,10 +1,43 @@
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
-import javax.swing.JPanel;
+import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener {
 
+    GamePanel() {
+
+    }
+
+    public void startGame() {
+
+    }
+    public void paintComponent(Graphics g) {
+
+    }
+    public void draw(Graphics g) {
+
+    }
+    public void move() {
+
+    }
+    public void checkApple() {
+
+    }
+    public void checkCollisions() {
+
+    }
+    public void gameOver(Graphics g) {
+
+    }
+
+    public class MyKeyAdapter extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+        }
+    }
     @Override
     public void actionPerformed(ActionEvent e) {
 
--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -2,5 +2,8 @@ public class SnakeGame {
 
     public static void main(String[] args) {
 
+        new GameFrame();
+
+
     }
 }
