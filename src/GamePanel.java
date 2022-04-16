c15fd48e1fc83d4af9e24c02b917904ae8e3ebfb
Zach Rickers <mzrickers@gmail.com>
2022-04-16 08:01:58 +0200

    class initialization

---
+++ src/GamePanel.java
@@ -0,0 +1,12 @@
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
